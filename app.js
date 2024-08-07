c1ba462ee22096a8ea6115d83f4876270f0911b9
Malaz Hasan <121643124+Malazhasan@users.noreply.github.com>
2024-08-07 06:48:29 -0400

    Update app.js

--- app.js
+++ app.js
@@ -82,15 +82,15 @@ app.use(express.json())
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true,
 // })
-mongoose
-  .connect("mongodb://localhost:27017/Hertz", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => console.error("MongoDB connection error:", err));
+// mongoose
+//   .connect("mongodb://localhost:27017/Hertz", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((err) => console.error("MongoDB connection error:", err));
 
 
   
